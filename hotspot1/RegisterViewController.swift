16b571b2c6857dd9cf99ec25b620f5b47f8ac778
jchoe <jchoe20@cmc.edu>
2018-10-23 19:18:08 -0700

    reg update

--- hotspot1/RegisterViewController.swift
+++ hotspot1/RegisterViewController.swift
@@ -1,35 +1,81 @@
 //
-//  RegisterViewController.swift
-//  hotspot1
+//  RegisterPageViewController.swift
+//  UserLoginAndRegistration
 //
-//  Created by Jacqueline Choe on 10/23/18.
-//  Copyright © 2018 CS121. All rights reserved.
+//  Created by Jacqueline Choe on 10/20/18.
+//  Copyright © 2018 G7S1. All rights reserved.
 //
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterPageViewController: UIViewController {
+    
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    
+    @IBOutlet weak var userConfirmPasswordTextField: UITextField!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        let userEmail = userEmailTextField.text;
+        let userPassword = userPasswordTextField.text;
+        let userConfirmPassword = userConfirmPasswordTextField.text;
+        
+        //Check for empty fields
+        if((userEmail?.isEmpty)! || (userPassword?.isEmpty)! || (userConfirmPassword?.isEmpty)!)
+        {
+            //Display alert message
+            displayMyAlertMessage(userMessage: "All fields are required");
+            
+            return;
+        }
+        
+        //Check if passwords match
+        if(userPassword != userConfirmPassword)
+        {
+            //Display alert message
+            displayMyAlertMessage(userMessage: "Passwords do not match");
+            return;
+        }
+        
+        
+        //Store Data
+        UserDefaults.standard.set(userEmail, forKey:"userEmail");
+        UserDefaults.standard.set(userPassword, forKey:"userPassword");
+        UserDefaults.standard.synchronize();
+        
+        //Display alert message with confirmation
+        var myAlert = UIAlertController(title:"Alert", message:"Registration is successful. Thank you!", preferredStyle:UIAlertControllerStyle.alert);
+        
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default){ action in
+            self.dismiss(animated: true, completion:nil);
+        }
+        myAlert.addAction(okAction);
+        self.present(myAlert, animated:true, completion:nil);
+        
     }
-    */
-
+    func displayMyAlertMessage(userMessage:String)
+    {
+        var myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle:UIAlertControllerStyle.alert);
+        
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil);
+        
+        myAlert.addAction(okAction);
+        
+        self.present(myAlert, animated: true, completion:nil);
+        
+        
+    }
+    
 }
