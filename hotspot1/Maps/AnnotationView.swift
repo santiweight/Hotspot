8c6014697bc73b03580487ad4951d679409333b6
zackrossman10 <zrossman20@cmc.edu>
2018-11-04 09:06:22 -0800

    deleted duplicate files, need map.storyboard

--- hotspot1/Maps/AnnotationView.swift
+++ hotspot1/Maps/AnnotationView.swift
@@ -15,7 +15,7 @@ class AnnotationView: MKAnnotationView
         let hitView = super.hitTest(point, with: event)
         if (hitView != nil)
         {
-            self.superview?.bringSubview(toFront: self)
+            self.superview?.bringSubviewToFront(self)
         }
         return hitView
     }
