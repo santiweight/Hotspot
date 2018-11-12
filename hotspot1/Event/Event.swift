dfd7f005255453990b0c78f840ef02f334c15368
Ryan Piantedosi <clinic18@WL-213-80.WPA.Claremont.Edu>
2018-11-12 14:25:32 -0800

    AWS models added, and updated Event class

--- hotspot1/Event/Event.swift
+++ hotspot1/Event/Event.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class Event{
     var _event_id:      Int!
+    var _user_id:       String!
     var _creator_email: String!
     var _title:         String!
     var _address:       String!
@@ -23,8 +24,9 @@ class Event{
     var _year_filters:  [String]!
     var _school_filters: [String]!
     
-    init(event_id: Int, creator_email: String, title: String, address: String, description: String, start: DateComponents, end: DateComponents, attendees: [String], expectedAttendees: Int, latitude: Double, longitude: Double, year_filters: [String], school_filters: [String]){
+    init(event_id: Int, user_id: String, creator_email: String, title: String, address: String, description: String, start: DateComponents, end: DateComponents, attendees: [String], expectedAttendees: Int, latitude: Double, longitude: Double, year_filters: [String], school_filters: [String]){
         _event_id      = event_id
+        _user_id       = user_id
         _creator_email = creator_email
         _title         = title
         _address       = address
@@ -37,6 +39,24 @@ class Event{
         _longitude     = longitude
         _year_filters  = year_filters
         _school_filters = school_filters
+    }
+    
+    init(){
+        _event_id      = 0
+        _user_id       = "NULL"
+        _creator_email = "NULL"
+        _title         = "NULL"
+        _address       = "NULL"
+        _description   = "NULL"
+        _start         = DateComponents()
+        _end           = DateComponents()
+        _attendees     = ["NULL"]
+        _expectedAttendees = 0
+        _latitude      = 0.0
+        _longitude     = 0.0
+        _year_filters  = ["NULL"]
+        _school_filters = ["NULL"]
+        
     }
     
     func setDate(start: DateComponents, end: DateComponents){
