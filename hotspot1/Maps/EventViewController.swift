ee2b0fda68e15d6a188d9514d1216adbbf88dc4a
Ethan Lewis <elewis16@punahou.edu>
2018-11-15 22:48:57 -0800

    added color scheme to most of the app, also add labels for single event page

--- hotspot1/Maps/EventViewController.swift
+++ hotspot1/Maps/EventViewController.swift
@@ -13,20 +13,40 @@ class EventViewController: UIViewController {
     var name: String? = ""
     var id2: String? = ""
     
+    var address: String? = "3920 Old Pali Road"
+    var time: String? = "10:00 AM - 11:00 AM"
+    var desc: String? = "Play date with the pugs, all are welcome"
+    var host: String? = "elewis20@stuents.claremontmckenna.edu"
+    
+    
 
     @IBOutlet weak var backButton: UIButton!
     
-    
+    // test = name
     @IBOutlet weak var test: UILabel!
     @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var hostLabel: UILabel!
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+        // attempting to set border around tittle of event
+        // test.layer.borderColor = UIColor.orange.cgColor
+        
+        
         test?.text = name
         idLabel?.text = id2
+        
+        // querery database for rest of info.
+        descLabel?.text = desc
+        addressLabel?.text = address
+        hostLabel?.text =  host
+        timeLabel?.text = time
     }
     
 
