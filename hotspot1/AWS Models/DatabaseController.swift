9dda5ff56e87e63fd1d20562db3806770791ae69
Dosi-Dough <33563704+Dosi-Dough@users.noreply.github.com>
2018-11-19 20:45:54 -0800

    Revert "Ryandev"

--- hotspot1/AWS Models/DatabaseController.swift
+++ hotspot1/AWS Models/DatabaseController.swift
@@ -9,7 +9,6 @@
 import Foundation
 import AWSCore
 import AWSDynamoDB
-import UIKit
 
 protocol DBInterface {
     func eventIdQuery(eventTitle: String)
@@ -24,14 +23,35 @@ class DatabaseController: DBInterface {
         //TODO
     }
     
-    obejectMapper.query(EventTable.self, expression: queryExpression, completionHandler:
-        {(response: AWSDynamoDBPaginatedOutput?, error: Error?) -> Void in
-            
-            if let error = error{
-                print("Amazon DynamoDB Save Error: \(error)")
-            }
-            DispatchQueue.main.sync(execute: {
+    func attendEvent(event: Event, attendee: String) {
+        //TODO
+    }
 
+    var deviceID = (UIDevice.current.identifierForVendor?.uuidString)!
+
+    func eventIdQuery(eventTitle: String){
+        
+        let obejectMapper = AWSDynamoDBObjectMapper.default()
+        let queryExpression = AWSDynamoDBQueryExpression()
+        
+        queryExpression.keyConditionExpression = "#userId = :userId and #title = :title"
+        queryExpression.expressionAttributeNames = [
+            "#userId": "userId",
+            "#title": "title",
+        ]
+        
+        queryExpression.expressionAttributeValues = [
+            ":userId" : deviceID,
+            ":title" : eventTitle,
+        ]
+        
+        obejectMapper.query(EventTable.self, expression: queryExpression, completionHandler:
+            {(response: AWSDynamoDBPaginatedOutput?, error: Error?) -> Void in
+                
+                if let error = error{
+                    print("Amazon DynamoDB Save Error: \(error)")
+                }
+                //DispatchQueue.main.async(execute: {
                 print("querying")
                 //got a response
                 if(response != nil){
@@ -40,49 +60,23 @@ class DatabaseController: DBInterface {
                     if(response?.items.count == 0){
                         print("count was 0")
                         //then take our object and put it in DB?
-                    }
-                    else {
-                        
-                        for item in (response?.items)! as! [EventTable]{
+                    } else {
+                        //var eventList = []
+                        for item in (response?.items)!{
                             //we found the objects we want
-                            let userEvent = Event()
-                            userEvent.queryObjToUserEvent(qObj: item)
-                            print(userEvent)
+                            if(item.value(forKey: "_userId") != nil){
+                                
+                                if let existingID = item.value(forKey: "_userId"){
+                                    print("item")
+                                    print(existingID)
+                                }
+                            }
+                        }
                     }
                 }
-            }
+                //})
         })
-    })
-}
-
-func getEvents(indexType: String, indexVal: String){
-    let scanExpression = AWSDynamoDBScanExpression()
-    scanExpression.limit = 50
-    let om = AWSDynamoDBObjectMapper.default()
-    
-    if(indexType != "ALL"){
-        scanExpression.filterExpression = indexType + " = :val"
-        scanExpression.expressionAttributeValues = [":val": indexVal]
     }
-    
-    om.scan(EventTable.self, expression: scanExpression).continueWith(block: { (task:AWSTask<AWSDynamoDBPaginatedOutput>!) -> Any? in
-        if let error = task.error as NSError? {
-            print("The request failed. Error: \(error)")
-        }
-        else if let paginatedOutput = task.result {
-            for event in paginatedOutput.items as! [EventTable] {
-                let userEvent = Event()
-                userEvent.queryObjToUserEvent(qObj: event)
-                
-                print(event)
-                //addEventToMap(userEvent)
-            }
-        }
-        return nil
-    })
-    
-}
-
 
     func updateEventDb(event: Event){
         
