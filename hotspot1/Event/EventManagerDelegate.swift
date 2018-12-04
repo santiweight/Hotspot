8e9f572f54bce92c240b6bb66012c9094129731c
santiweight <sweight19@cmc.edu>
2018-12-03 19:09:42 -0800

    EventManagerTestPage

---
+++ hotspot1/Event/EventManagerDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  EventManagerDelegate.swift
+//  hotspot1
+//
+//  Created by cs121 on 12/2/18.
+//  Copyright © 2018 CS121. All rights reserved.
+//
+
+import Foundation
---
+++ hotspot1/Event/EventManagerTestPage.swift
@@ -0,0 +1,9 @@
+//
+//  EventManagerTestPage.swift
+//  hotspot1
+//
+//  Created by cs121 on 11/30/18.
+//  Copyright © 2018 CS121. All rights reserved.
+//
+
+import Foundation
